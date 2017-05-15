fb48a02c76ac7298e970f028775dc231ce0597d0
qianjun.yang <ivugogo@gmail.com>
2017-05-15 17:33:12 +0800

    0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -9,26 +9,26 @@ let babelRc = JSON.parse(fs.readFileSync('./.babelrc'));
 let entry = process.env.ENTRY || 'index';
 
 export default {
-	entry: 'src/'+entry+'.js',
-	exports: 'default',
-	useStrict: false,
-	external: ['preact'],
-	plugins: [
-		memory({
-			path: 'src/'+entry+'.js',
-			contents: "export { default } from './"+entry+"';"
-		}),
-		nodeResolve({
-			skip: ['preact'],
-			main: true
-		}),
-		commonjs(),
-		babel({
-			babelrc: false,
-			comments: false,
-			exclude: [],
-			presets: ['es2015-minimal-rollup'].concat(babelRc.presets.slice(1)),
-			plugins: babelRc.plugins
-		})
-	]
+    entry: 'src/'+entry+'.js',
+    exports: 'default',
+    useStrict: false,
+    external: ['qreact'],
+    plugins: [
+        memory({
+            path: 'src/'+entry+'.js',
+            contents: "export { default } from './"+entry+"';"
+        }),
+        nodeResolve({
+            skip: ['qreact'],
+            main: true
+        }),
+        commonjs(),
+        babel({
+            babelrc: false,
+            comments: false,
+            exclude: [],
+            presets: ['es2015-minimal-rollup'].concat(babelRc.presets.slice(1)),
+            plugins: babelRc.plugins
+        })
+    ]
 };
